f6bfe34f1dd83f4c7b3967e0a026147549450896
m2288 <ncniyazov@gmail.com>
2021-12-30 12:07:03 +0400

    Visual chages, not paste function yet

--- Youtube-dwn.py
+++ Youtube-dwn.py
@@ -1,24 +1,53 @@
 from tkinter import *
 from pytube import YouTube
+import clipboard
+
 root = Tk()
-root.geometry('500x300')
-root.resizable(0,0)
+root.geometry("500x240")
+root.resizable(0, 0)
 root.title("Enigma-youtube video downloader")
-Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
-link = StringVar()
+Label(root, text="Youtube Video Downloader", font="arial 20 bold").pack()
+yt_link = StringVar()
 
 
-Label(root, text = 'Youtube linki:', font = 'arial 15 bold').place(x= 160 , y = 60)
-link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+def _linkPaste():
+    t = clipboard.paste()
+    yt_link=t
 
-def Downloader(): 
-    Label(root, text = 'Yükləndi', font = 'arial 15').place(x= 180 , y = 210)     
-    url =YouTube(str(link.get()))
-    video = url.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
+Label(root, text="Youtube linki:", font="arial 15 bold").place(x=160, y=60)
+link_enter = Entry(root, width=55, textvariable=yt_link).place(x=32, y=90)
+Button(
+    root,
+    text="Youtube linkini qoy",
+    font="arial 10",
+    bg="#4A7A8C",
+    padx=2,
+    command=_linkPaste,
+).place(x=370, y=85)
+
+
+def Downloader():
+    Label(root, text="Yükləndi", font="arial 15").place(x=180, y=210)
+    url = YouTube(str(yt_link.get()))
+    video = (
+        url.streams.filter(progressive=True, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+    )
     print(video)
     video.download()
-    Label(root, text = 'Yükləndi', font = 'arial 15').place(x= 180 , y = 210)  
+    Label(root, text="Yükləndi", font="arial 15").place(x=180, y=210)
 
-Button(root,text = 'Yüklə', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+
+Button(
+    root,
+    text="Yüklə",
+    font="arial 15 bold",
+    bg="#4A7A8C",
+    padx=2,
+    command=Downloader,
+).place(x=210, y=150)
 
 root.mainloop()
